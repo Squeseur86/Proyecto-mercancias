bf9708621d22172f86611761d857f62c54ec8626
Juan <juan.merchan04@uptc.edu.co>
2024-02-13 20:32:46 -0500

    Cumplimiento a: Verificar la creación de la cuenta, Generacion de usuario random. Con metodos en usuarioController para cada una de los requerimientos

--- src/Controller/usuarioController.java
+++ src/Controller/usuarioController.java
@@ -1,13 +1,39 @@
 package Controller;
 
+import java.util.ArrayList;
 import java.util.Date;
 
 import Model.Telefono;
 import Model.Usuario;
 
 public class usuarioController {
-    public Usuario crearUsuario(String id, String userName, String email, Telefono phoneNumber, String dni,
+
+    private ArrayList<Usuario> usuarios;
+    
+    public void crearUsuario(String id, String userName, String email, Telefono phoneNumber, String dni,
             String fullName, String password, Date createdAT, Date updateAT) {
-        return new Usuario(id, userName, email, phoneNumber, dni, fullName, password, createdAT, updateAT);
+        usuarios.add(new Usuario(id, userName, email, phoneNumber, dni, fullName, password, createdAT, updateAT));
+    }
+
+    public boolean verificarUsuario(String userName, String password){
+        if(usuarios.size() == 0) {
+            return false;
+        }else{
+            for(Usuario usuario: usuarios){
+                if(usuario.getUserName().equals(userName) && usuario.getPassword().equals(password)){
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+
+    public boolean existenciaUserName(String userName){
+        for(Usuario usuario: usuarios){
+            if(usuario.getUserName().equals(userName)){
+                return true;
+            }
+        }
+        return false;
     }
 }
