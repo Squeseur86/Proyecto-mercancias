4dd24b6fd3b28998632843e322f5062d034dd21f
Juan <juan.merchan04@uptc.edu.co>
2024-02-25 12:13:18 -0500

    Creacion funcionalidad aceptar oferta. Gestiona el peso y espacio disponible en una publicacion

--- src/Controller/PublicacionController.java
+++ src/Controller/PublicacionController.java
@@ -3,8 +3,8 @@ package Controller;
 import java.util.ArrayList;
 import java.util.Date;
 
+import Model.Oferta;
 import Model.Publicacion;
-import Model.Usuario;
 
 public class PublicacionController {
 
@@ -17,5 +17,26 @@ public class PublicacionController {
 
 	public ArrayList<Publicacion> listarPublicaciones() {
 		return publicaciones;
+	}
+
+	public String aceptarOferta(Oferta oferta, Publicacion publicacionModi){
+		double pesoDisponiblePublicacion = Double.parseDouble(publicacionModi.getPesoEquipaje());
+		double espacioDisponiblePublicacion = Double.parseDouble(publicacionModi.getEspacioEquipaje());
+		double pesoOferta = Double.parseDouble(oferta.getTamaño());
+		double nuevoPesoPublicacion;
+		if(pesoDisponiblePublicacion == 0.0 || espacioDisponiblePublicacion == 0.0){
+			return "No hay espacio disponible."; 
+		}
+		for(Publicacion publicacion : publicaciones){
+			if(publicacionModi.equals(publicacion)){
+				nuevoPesoPublicacion = pesoDisponiblePublicacion - pesoOferta;
+				if(nuevoPesoPublicacion < 0.0){
+					return "No hay espacio suficiente";
+				}
+				publicacion.setPesoEquipaje(String.valueOf(nuevoPesoPublicacion)); 
+				return "Oferta procesada correctamente";
+			}
+		}
+		return "Publicacion inexistente";
 	}
 }
